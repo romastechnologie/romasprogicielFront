875a779aa8e0248314a9760c2f6c1cf6e7703b6d
Arie158 <alavoariel264@gmail.com>
2024-05-13 21:14:46 +0100

    1305242114

---
+++ src/pages/users/indexCreateUser.vue
@@ -0,0 +1,15 @@
+<template>
+    .
+    <div class="container-fluid">
+        <div class="row">
+            <Card3 colClass="col-sm-12">
+                <NewUser />
+            </Card3>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref, defineAsyncComponent } from 'vue'
+const NewUser = defineAsyncComponent(() => import("@/components/theme/users/createuser/NewUser.vue"))
+const Card3 = defineAsyncComponent(() => import("@/components/common/card/CardData3.vue"))
+</script>
